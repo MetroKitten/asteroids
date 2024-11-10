2b372e487c057a1f821eb758b5f8a3043225f985
MetroKitten <113661103+MetroKitten@users.noreply.github.com>
2024-11-10 20:14:00 +1100

    tick rate

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from constants import *
 def main():
     pygame.init()
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -15,6 +15,8 @@ def main():
                 return
         pygame.Surface.fill(screen,color=(0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
