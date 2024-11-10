137fa3d7874da4dfb8639759b74544683d6746bd
MetroKitten <113661103+MetroKitten@users.noreply.github.com>
2024-11-10 21:15:11 +1100

    leekspin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
@@ -8,12 +10,15 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    protag = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,color=(0, 0, 0))
+        screen.fill(color=(0, 0, 0))
+        protag.draw(screen)
+        protag.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
